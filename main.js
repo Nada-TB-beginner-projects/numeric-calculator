d21d28f711e156873260242924d668cf9486e354
Nada <nada.tebba@hotmail.fr>
2019-09-20 23:47:58 +0200

    simple way to create numeric caliculator using eval()

---
+++ main.js
@@ -0,0 +1,36 @@
+'use strict;'
+
+/****************************variables****************************/
+
+var outPut=document.getElementById('outPut');
+const clearbtn= document.getElementById("clear");
+const resultbtn=document.getElementById("result");
+var number=document.getElementsByClassName("target");
+
+/**************************functions******************************/
+
+function clear(){
+	outPut.value= "";			
+}
+
+function btnClick(){
+	outPut.value+= this.value;	
+}
+
+function result(){
+	var result;
+	result=eval(outPut.value);
+	outPut.value=result;	
+}
+
+
+/**************************Events**********************************/
+clearbtn.addEventListener('click', clear);
+for (var i = 0; i < number.length; i++) {
+	number[i].addEventListener('click', btnClick);
+}
+
+resultbtn.addEventListener('click', result);
+
+
+
